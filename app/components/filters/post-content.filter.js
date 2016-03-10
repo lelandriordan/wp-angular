c47070ddd9d1ff556c59978915c7148588a0d6ca
Leland Riordan <leland@lelandriordan.com>
2016-03-10 09:35:31 -0500

    Updated Post Content Filter

--- app/components/filters/post-content.filter.js
+++ app/components/filters/post-content.filter.js
@@ -2,22 +2,22 @@ angular
   .module('app.filters.postContentFilter', [])
   .filter('postContentFilter', postContentFilter);
 
+/**
+ * Example filter for manipulating WP REST API HTML response
+ */
 function postContentFilter($sce) {
 
   return function(text) {
 
     var htmlObject = document.createElement('div');
     htmlObject.innerHTML = text;
-
     var links = htmlObject.getElementsByTagName('a');
 
     for (var i = 0; i < links.length; i++) {
 
       var link = links[i].getAttribute('href');
 
-      links[i].setAttribute('onclick', 'window.open("' + link + '", "_system", "location=no,enableViewportScale=yes")');
-
-      links[i].removeAttribute('href');
+      // Manipulate the link here
     }
 
     return $sce.trustAsHtml(htmlObject.outerHTML);
